b08d726c2e7110481739cb51d8146b62dc7e9c1a
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-11-12 15:00:47 -0700

    Fixed price bug

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 5000;
 
 const calculateRate = (req, res) => {
     const { weight, mail_type } = req.body;
-    const price = weight > letterConversion[mail_type].maxWeight ? letterConversion[mail_type].maxWeight.toFixed(2) : letterConversion[mail_type][weight].toFixed(2);
+    const maxWeight = letterConversion[mail_type].maxWeight;
+    const price = weight > maxWeight ? letterConversion[mail_type][maxWeight].toFixed(2) : letterConversion[mail_type][weight].toFixed(2);
     const mail_name = letterConversion[mail_type].name;
     res.render('pages/result.ejs', { price, weight, mail_name });
 }
